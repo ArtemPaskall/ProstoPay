8c96750ea924f9b9d20a9d7b929166d9c97dc9c1
ArtemPaskall <artempaskal@gmail.com>
2022-10-11 15:39:41 +0300

    Images mobile and desktop changed

--- src/scripts/video-open.js
+++ src/scripts/video-open.js
@@ -6,28 +6,31 @@ const video = document.getElementById('video');
 
 showButtons.forEach(showButton => {
   showButton.addEventListener('click', showVideo);
-})
+});
 
 function showVideo() {
   const videoSection = document.querySelector('.page__section--video')
 
   if (!videoSection.classList.contains('page__section--video-open')) {
-    videoSection.classList.add('page__section--video-open')
-    setTimeout(() => {
-      video.style.zIndex = "1";
-    }, 400)
+    videoSection.classList.add('page__section--video-open');
 
     setTimeout(() => {
-      videoSection.style.position = "relative"
-    }, 150)
+      video.style.zIndex = '1';
+    }, 400);
+
+    setTimeout(() => {
+      videoSection.style.position = 'relative';
+    }, 150);
   }
 }
 
 closeButton.addEventListener('click', hideVideo)
 
 function hideVideo() {
-  video.style.zIndex = "-1";
-  const videoSection = document.querySelector('.page__section--video')
-  videoSection.classList.remove('page__section--video-open')
-  videoSection.style.position = "absolute"
+  video.style.zIndex = '-1';
+
+  const videoSection = document.querySelector('.page__section--video');
+
+  videoSection.classList.remove('page__section--video-open');
+  videoSection.style.position = 'absolute';
 }
