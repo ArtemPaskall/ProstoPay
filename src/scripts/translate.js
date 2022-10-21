2f7558d9b0e8bcfe229d149eac1e078b44cad825
ArtemPaskall <artempaskal@gmail.com>
2022-10-21 12:39:59 +0300

    About us fixes

--- src/scripts/translate.js
+++ src/scripts/translate.js
@@ -91,20 +91,6 @@ export const translationArr = {
     'UA': 'Про нас',
     'EN': 'About Us',
   },
-  // 'about-text': {
-  //   'UA': 'ProstoPay був створений в Україні. Простота керує нашим баченням. Ми робимо складні ІТ-рішення простими у використанні для партнерів і клієнтів.'
-  //   + 'Наша місія допомагати кавовим і вендинговим компаніям вести свій бізнес на новому рівні операційної ефективності та продуктивності,'
-  //    + 'а також насолоджуватися зростанням доходу завдяки сучасним платіжним рішенням.<br />'
-  //   + 'Ми почали в 2016 році з простим рішенням для RFID-карт на автоматах з продажу снеків.'
-  //    + 'Це був лише початок, далі ми продовжили зростати, створюючи нові IoT-пристрої та програмні продукти.'
-  //     + 'Власна команда розробників (і фізичних пристроїв, і програмних продуктів) надає різноманітні гнучкі клієнтоорієнтовані рішення.'
-  //     + 'Сьогодні наші продукти підтримують телеметрію та безготівкові платежі для кави, снеків, автоматів з розливу води, їжї, іграшок, автомийок та на інших торгових апаратах.<br />'
-  //   + 'Не варто забувати і про нашу підтримку: ProstoPay не зміг би розвиватися без швидкої та якісної підтримки клієнтів.'
-  //   + 'Ми пишаємося тим, що завжди готові прийти на допомогу і вирішити будь-яку проблему для наших партнерів і клієнтів. Обираючи ProstoPay, ви отримуєте сучасний продукт, швидку та якісну техпідтримку, конфіденційність даних та надійні і дружні відносини.',
-  //   'EN': 'ProstoPay was born in Ukraine. “Prosto” means “simple” in Ukrainian. Simplicity is driving our vision - to remove the barriers in coffee and snack consumption. We make complicated IT solutions easy to use for partners and customers. We see our mission in helping coffee and vending companies to run their business at the new level of operational efficiency and productivity and to enjoy higher sales driven by modern payment solutions.'
-  //   + 'We started in 2016 as an ordinary RFID card solution for snack vending machines. It was just the beginning and we continued to develop our product. We’ve been growing, creating new IoT devices and software . In-house hardware & software teams provide variety of flexible client-oriented solutions. Today our products support telemetry and cashless payments for coffee, snack, water, food,  toys, car washes and other vending machines.'
-  //   + 'We should not forget about our customer service team: ProstoPay couldn’t evolve without fast and quality customer support. We are proud to be always ready to assist and resolve any problem for our partners and customers.',
-  // },
   'about-text-p1': {
     'UA': '&emsp; ProstoPay був створений в Україні. Простота керує нашим баченням.'
      + ' Ми робимо складні ІТ-рішення простими у використанні для партнерів і клієнтів.'
@@ -118,7 +104,7 @@ export const translationArr = {
   'about-text-p2': {
     'UA': '&emsp; Ми почали в 2016 році з простим рішенням для RFID-карт на автоматах з продажу снеків.'
      + ' Це був лише початок, далі ми продовжили зростати, створюючи нові IoT-пристрої та програмні продукти.'
-     + ' Власна команда розробників (і фізичних пристроїв, і програмних продуктів) надає різноманітні гнучкі клієнтоорієнтовані рішення.'
+    //  + ' Власна команда розробників (і фізичних пристроїв, і програмних продуктів) надає різноманітні гнучкі клієнтоорієнтовані рішення.'
      + ' Сьогодні наші продукти підтримують телеметрію та безготівкові платежі для кави, снеків, автоматів з розливу води, їжї, іграшок, автомийок та на інших торгових апаратах.',
     'EN': '&emsp; We started in 2016 as an ordinary RFID card solution for <strong>snack vending machines</strong>.'
      + ' It was just the beginning and we continued to develop our product. We’ve been growing, creating new IoT devices and software.'
