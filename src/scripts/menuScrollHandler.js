e4a874de4c80a934e721ba677f8875b7f74f3ac6
ArtemPaskall <artempaskal@gmail.com>
2022-10-30 22:59:44 +0200

    solutin

--- src/scripts/menuScrollHandler.js
+++ src/scripts/menuScrollHandler.js
@@ -1,5 +1,42 @@
 // Hide header menu on scroll down, show on scroll up
 
+// let lastScrollTop = 0;
+// const menu = document.getElementById('menu');
+
+// window.addEventListener('scroll', function() {
+//   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+//   const menustyle = this.getComputedStyle(menu);
+//   const height = menustyle.height;
+
+//   menu.style.position = 'sticky';
+//   menu.style.position = 'absolute';
+//   menu.style.top = '-400px';
+
+//   if (scrollTop > lastScrollTop) {
+//     const touchForm2 = document.getElementById('touch-form-2');
+
+//     if (touchForm2.classList.contains('touch-form--active')) {
+//       menu.style.position = 'sticky';
+//     } else {
+//       menu.style.top = '-' + height;
+//     }
+//   } else {
+//     menu.style.top = '0';
+//     menu.style.zIndex = '1';
+
+//     if (scrollTop < 200) {
+//       menu.style.top = '-400px';
+//       menu.style.position = 'absolute';
+//       menu.style.zIndex = '-4';
+//     } else {
+//       menu.style.position = 'sticky';
+//     }
+//   }
+
+//   lastScrollTop = scrollTop;
+// });
+
 let lastScrollTop = 0;
 const menu = document.getElementById('menu');
 
@@ -9,9 +46,6 @@ window.addEventListener('scroll', function() {
   const menustyle = this.getComputedStyle(menu);
   const height = menustyle.height;
 
-  menu.style.position = 'absolute';
-  menu.style.top = '-400px';
-
   if (scrollTop > lastScrollTop) {
     const touchForm2 = document.getElementById('touch-form-2');
 
@@ -20,9 +54,14 @@ window.addEventListener('scroll', function() {
     } else {
       menu.style.top = '-' + height;
     }
+    menu.style.top = '-' + modifyHeight(height);
+    menu.style.boxShadow = 'none';
+    menu.style.marginTop = '-1px';
   } else {
     menu.style.top = '0';
     menu.style.zIndex = '1';
+    menu.style.position = 'sticky';
+    menu.style.boxShadow = '0 4px 4px rgba(0, 0, 0, 0.25)';
 
     if (scrollTop < 200) {
       menu.style.top = '-400px';
@@ -34,4 +73,10 @@ window.addEventListener('scroll', function() {
   }
 
   lastScrollTop = scrollTop;
+
+  function modifyHeight(h) {
+    const newHeight = +(h.slice(0, -2)) + 1;
+
+    return `${newHeight}px`;
+  }
 });
--- src/scripts/menuScrollHandlerNotIndex.js
+++ src/scripts/menuScrollHandlerNotIndex.js
@@ -1,5 +1,28 @@
 // Hide header menu on scroll down, show on scroll up
 
+// let lastScrollTop = 0;
+// const menu = document.getElementById('menu');
+
+// window.addEventListener('scroll', function() {
+//   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+//   const menustyle = this.getComputedStyle(menu);
+//   const height = menustyle.height;
+
+//   menu.style.position = 'relative';
+//   menu.style.top = '-150px';
+
+//   if (scrollTop > lastScrollTop) {
+//     menu.style.top = '-' + height;
+//   } else {
+//     menu.style.top = '0';
+//     menu.style.zIndex = '2';
+//     menu.style.position = 'sticky';
+//   }
+
+//   lastScrollTop = scrollTop;
+// });
+
 let lastScrollTop = 0;
 const menu = document.getElementById('menu');
 
@@ -9,15 +32,14 @@ window.addEventListener('scroll', function() {
   const menustyle = this.getComputedStyle(menu);
   const height = menustyle.height;
 
-  menu.style.position = 'relative';
-  menu.style.top = '-150px';
-
   if (scrollTop > lastScrollTop) {
     menu.style.top = '-' + height;
+    menu.style.boxShadow = 'none';
   } else {
     menu.style.top = '0';
     menu.style.zIndex = '2';
     menu.style.position = 'sticky';
+    menu.style.boxShadow = '0 4px 4px rgba(0, 0, 0, 0.25)';
   }
 
   lastScrollTop = scrollTop;
